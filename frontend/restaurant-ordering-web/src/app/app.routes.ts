d58b05c3000475b843a7562e745502406ea0bfde
Abdallah_Hijazi <AbdallahHijazi666@gmail.com>
2026-06-01 02:32:48 +0300

    chore(frontend): initialize angular workspace and routing skeleton

---
+++ frontend/restaurant-ordering-web/src/app/app.routes.ts
@@ -0,0 +1,53 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'r/demo/menu',
+  },
+  {
+    path: 'r/:restaurantSlug',
+    loadComponent: () =>
+      import('./core/layouts/public-layout/public-layout')
+        .then((m) => m.PublicLayout),
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'menu',
+      },
+      {
+        path: 'menu',
+        loadComponent: () =>
+          import('./features/public-menu/pages/menu-page/menu-page')
+            .then((m) => m.MenuPage),
+      },
+    ],
+  },
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('./core/layouts/admin-layout/admin-layout')
+        .then((m) => m.AdminLayout),
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'dashboard',
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./features/admin/pages/dashboard-page/dashboard-page')
+            .then((m) => m.DashboardPage),
+      },
+    ],
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./shared/pages/not-found-page/not-found-page')
+        .then((m) => m.NotFoundPage),
+  },
+];
